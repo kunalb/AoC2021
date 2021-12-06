56b687d3e04202bd7405e7ca33e41ea8956b283b
Kunal Bhalla <bhalla.kunal@gmail.com>
2021-12-06 00:31:43 -0500

    Cleaned up

--- js/day06.js
+++ js/day06.js
@@ -1,54 +1,28 @@
 import {readLines} from "https://deno.land/std/io/mod.ts";
 
-if (Deno.args.length == 0 || Deno.args[0] == "1") {
-} else {
-}
-
 const lines = []
 for await (const l of readLines(Deno.stdin)) {
     lines.push(l);
 }
-
 const lanterns = lines[0].split(",").map(x => parseInt(x, 10));
 
-const cache = new Map();
+function countLanterns(lanterns, days) {
+    const lanternCounts = new Array(9).fill(0);
+    for (const lantern of lanterns) {
+        lanternCounts[lantern]++;
+    }
 
-function countLanterns(timer, totalDays) {
-    let lanterns = [timer];
-    while (totalDays > 0) {
-        let nextLanterns = [];
-        for (let lantern of lanterns) {
-            if (lantern == 0) {
-                nextLanterns.push(6);
-                nextLanterns.push(8);
-            } else {
-                nextLanterns.push(lantern - 1);
-            }
+    for (let i = 0; i < days; i++) {
+        const finished = lanternCounts[0];
+        for (let j = 0; j < 8; j++) {
+            lanternCounts[j] = lanternCounts[j + 1];
         }
-        lanterns = nextLanterns;
-
-        totalDays--;
+        lanternCounts[8] = finished;
+        lanternCounts[6] += finished;
     }
 
-    return lanterns.length;
+    return lanternCounts.reduce((a, b) => a + b);
 }
 
-function countLanterns2(lantern, totalDays) {
-    let key = `${lantern},${totalDays}`;
-    if (cache.has(key)) {
-        return cache.get(key);
-    }
-
-    let result;
-    if (totalDays == 0) {
-        result = 1;
-    } else if (lantern == 0) {
-        result = countLanterns2(6, totalDays - 1) + countLanterns2(8, totalDays - 1);
-    } else {
-        result = countLanterns2(lantern - 1, totalDays - 1);
-    }
-    cache.set(key, result);
-    return result;
-}
-
-console.log(lanterns.map(x => countLanterns2(x, 256)).reduce((a, b) => a + b));
+const days = (Deno.args.length == 0 || Deno.args[0] == "1") ? 80 : 256;
+console.log(countLanterns(lanterns, days));
