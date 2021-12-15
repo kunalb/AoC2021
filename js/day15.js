3e50b2901974ce30f8c4d906c4c42d6d79c0a625
Kunal Bhalla <bhalla.kunal@gmail.com>
2021-12-15 01:42:18 -0500

    Slightly cleaned up

--- js/day15.js
+++ js/day15.js
@@ -2,104 +2,48 @@ import { readLines } from "https://deno.land/std/io/mod.ts";
 
 const isPart1 = (Deno.args.length == 0 || Deno.args[0] == "1");
 
-const lines = [];
+let grid = [];
 for await (const l of readLines(Deno.stdin)) {
-  lines.push(l.split("").map((x) => parseInt(x, 10)));
+  grid.push(l.split("").map((x) => parseInt(x, 10)));
 }
 
-let x = 0;
-let y = 0;
-let X = lines[0].length;
-let Y = lines.length;
-
-let cache = new Map();
-let visited = new Set();
-
-/*
-function leastRisk(x1, y1) {
-  let key = `${x1}:${y1}`;
-
-  if (cache.has(key)) {
-    return cache.get(key);
-  }
-
-  if (x1 == X - 1 && y1 == Y - 1) {
-    return lines[y1][x1];
-  }
-
-  let options = [];
-  let pts = [
-    [x1 -1 , y1],
-    [x1 + 1, y1],
-    [x1, y1 -1 ],
-    [x1, y1 + 1],
-  ]
-
-  for (let pt of pts) {
-    let ptKey = `${pt[0]}:${pt[1]}`;
-    if (visited.has(ptKey)) {
-      continue;
-    }
-
-    if (pt[0] >= 0 && pt[0] < X && pt[1] >= 0 && pt[1] < Y) {
-      options.push(lines[pt[1]][pt[0]] + leastRisk(pt[0], pt[1]));
-    }
-  }
-
-  let result = Math.min.apply(null, options);
-  cache.set(key, result);
-  return result;
-}
-*/
-
-function dp(grid) {
-  let X = grid[0].length;
-  let Y = grid.length;
-
-  let table = new Array(Y).fill(0).map(() => new Array(X).fill(0));
-  let path = new Array(Y).fill(0).map(() => new Array(X).fill(null));
-
-  table[0][0] = 0;
-  path[0][0] = [[0, 0]];
-  for (let x = 1; x < X; x++) {
-    table[0][x] = table[0][x - 1] + grid[0][x];
-    path[0][x] = [...path[0][x - 1], [x, 0]];
-  }
-  for (let y = 1; y < Y; y++) {
-    table[y][0] = table[y - 1][0] + grid[y][0];
-    path[y][0] = [...path[y - 1][0], [0, y]];
-  }
-
-  for (let y = 1; y < Y; y++) {
-    for (let x = 1; x < X; x++) {
-      table[y][x] = grid[y][x] + Math.min(
-        table[y - 1][x],
-        table[y][x - 1],
-      );
-
-      if (table[y - 1][x] < table[y][x - 1]) {
-        path[y][x] = [...path[y - 1][x], [x, y]];
-      } else {
-        path[y][x] = [...path[y][x - 1], [x, y]];
+if (!isPart1) {
+  const X = grid[0].length;
+  const Y = grid.length;
+  const grid2 = new Array(Y * 5).fill(0).map(() => new Array(X * 5).fill(0));
+  for (let yy = 0; yy < 5; yy++) {
+    for (let xx = 0; xx < 5; xx++) {
+      const xOff = xx * X;
+      const yOff = yy * Y;
+      for (let y = 0; y < Y; y++) {
+        for (let x = 0; x < X; x++) {
+          const res = grid[y][x] + xx + yy;
+          grid2[yOff + y][xOff + x] = (res >= 10) ? res - 9 : res;
+        }
       }
     }
   }
-
-  return [table[Y - 1][X - 1], path[Y - 1][X - 1]];
+  grid = grid2;
 }
 
-let grid = lines;
-let grid2 = new Array(Y * 5).fill(0).map(() => new Array(X * 5).fill(0));
-for (let yy = 0; yy < 5; yy++) {
-  for (let xx = 0; xx < 5; xx++) {
-    let xOff = xx * X;
-    let yOff = yy * Y;
-    for (let y = 0; y < Y; y++) {
-      for (let x = 0; x < X; x++) {
-        let res = grid[y][x] + xx + yy;
-        grid2[yOff + y][xOff + x] = (res >= 10) ? res - 9 : res;
-      }
-    }
+// TODO
+class Heap {
+  constructor(grid) {
+    this.grid = grid;
+    this.X = grid[0].length;
+    this.Y = grid.length;
+    this.tree = new Array(X * Y);
+  }
+
+  add(x, y) {
+  }
+
+  update(x, y) {
+  }
+
+  pop() {
+    let res = this.tree[0];
+    return res;
   }
 }
 
@@ -125,7 +69,7 @@ function djikstras(grid) {
     for (let [dx, dy] of ds) {
       let xx = x + dx;
       let yy = y + dy;
-      if (xx < 0 || yy < 0 || xx >= X || yy >= Y) {
+      if (xx < 0 || yy < 0 || xx >= X || yy >= Y || visited[yy][xx]) {
         continue;
       }
 
@@ -139,11 +83,9 @@ function djikstras(grid) {
     let best = Number.MAX_SAFE_INTEGER;
     for (let y = 0; y < Y; y++) {
       for (let x = 0; x < X; x++) {
-        if (!visited[y][x]) {
-          if (table[y][x] < best) {
-            best = table[y][x];
-            current = [x, y];
-          }
+        if (!visited[y][x] && table[y][x] < best) {
+          best = table[y][x];
+          current = [x, y];
         }
       }
     }
@@ -152,13 +94,4 @@ function djikstras(grid) {
   return table[Y - 1][X - 1];
 }
 
-grid = isPart1 ? grid : grid2;
-// let [res, path] = dp(grid);
-// for (let y in grid) {
-//   let row = grid[y];
-//   for (let xx of path.filter(([xx, yy]) => yy == y).map(([xx, yy]) => xx)) {
-//     row[xx] = ".";
-//   }
-//   console.log(row.join(""));
-// }
 console.log(djikstras(grid));
