4f6352ea65c08cf3c2b045c8c6abfa108a72a3c5
Kunal Bhalla <bhalla.kunal@gmail.com>
2021-12-21 01:49:44 -0500

    Day 21

--- js/day21.js
+++ js/day21.js
@@ -2,42 +2,78 @@ import { readLines } from "https://deno.land/std/io/mod.ts";
 
 const isPart1 = (Deno.args.length == 0 || Deno.args[0] == "1");
 
-// const lines = [];
-// for await (const l of readLines(Deno.stdin)) {
-//     lines.push(l);
-// }
+let posns = [];
+for await (const l of readLines(Deno.stdin)) {
+  posns.push(l);
+}
+posns = posns.map(x => x.split(":")[1])
+  .map(x => x.trim())
+  .map(x => parseInt(x, 10))
+  .map(x => (x - 1) % 10);
 
-// 1 - > 10 wraps around
-// scores start at 0
-// ends at at least 1000
-// 100 sided die, 1..100
 
-let p = [8 - 1, 7 - 1];
-// let p = [4 - 1, 8 - 1];
+if (isPart1) {
+  const scores = [0, 0];
+  let rolls = 0;
+  let player = 0;
 
-let scores = [0, 0];
-let rolls = 0;
-let die = 1;
+  let die = 1;
 
-let player = 0;
+  out:
+  while (true) {
+    for (let i = 0; i < 3; i++) {
+      posns[player] += die;
+      posns[player] = posns[player] % 10;
+      // console.log(die, player, p[player]);
 
-out:
-while (true) {
-  for (let i = 0; i < 3; i++) {
-    p[player] += die;
-    p[player] = p[player] % 10;
-   // console.log(die, player, p[player]);
+      die++;
+      die = die > 100 ? 1 : die;
+      rolls++;
+    }
+    scores[player] += posns[player] + 1;
+    if (scores[player] >= 1000) {
+      console.log(rolls * scores[1 - player]);
+      break out;
+    }
 
-    die++;
-    die = die > 100 ? 1 : die;
-    rolls++;
+    player = 1 - player;
   }
-  scores[player] += (p[player] + 1);
-  if (scores[player] >= 1000) {
-    console.log(rolls * scores[1 - player]);
-    break out;
+} else {
+  const memo = new Map();
+
+  function countWins(posns, totals, p) {
+    const key = `${posns}/${totals}/${p}`;
+    if (memo.has(key)) {
+      return memo.get(key);
+    }
+
+    const wins = [0, 0];
+
+    for (let i = 0; i < 27; i++) {
+      const posn = posns[p];
+      const total = totals[p];
+
+      const rollsSum = [...Array(3).keys()]
+            .map(x => 1 + Math.floor(i / (3 ** x)) % 3)
+            .reduce((a, b) => a + b)
+      posns[p] = (posn + rollsSum) % 10;
+      totals[p] -= (posns[p] + 1);
+
+      if (totals[p] <= 0) {
+        wins[p]++;
+      } else {
+        const innerWins = countWins(posns, totals, 1 - p);
+        wins[0] += innerWins[0];
+        wins[1] += innerWins[1];
+      }
+
+      posns[p] = posn;
+      totals[p] = total;
+    }
+
+    memo.set(key, wins);
+    return wins;
   }
 
-  // console.log(p, scores, rolls);
-  player = 1 - player;
+  console.log(Math.max(...countWins(posns, [21, 21], 0)));
 }
